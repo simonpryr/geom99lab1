55d45006094462ef933fbee86e3c94b142984679
simonpryr <148002942+simonpryr@users.noreply.github.com>
2024-01-23 23:23:24 -0500

    Update index.js

--- story/index.js
+++ story/index.js
@@ -1,7 +1,7 @@
 // This example displays a marker at the center of Australia.
 // When the user clicks the marker, an info window opens.
 function initMap() {
-  const uluru = { lat: -25.363, lng: 131.044 };
+  const uluru = { lat: 28.2096, lng: 83.9856 };
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 4,
     center: uluru,
