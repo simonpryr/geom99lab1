4ad483dd1da574f8d0dbc86f32e3179ab9238414
simonpryr <148002942+simonpryr@users.noreply.github.com>
2024-01-24 00:12:19 -0500

    Update index.js

--- story/index.js
+++ story/index.js
@@ -3,7 +3,7 @@
 function initMap() {
   const uluru = { lat: 27.9881, lng: 86.9250 };
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 7,
+    zoom: 20,
     center: uluru,
   });
   const contentString =
