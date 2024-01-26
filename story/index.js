69d0c500d5fe236fd8fa3b5e6ca3368eedd68315
simonpryr <148002942+simonpryr@users.noreply.github.com>
2024-01-25 23:04:49 -0500

    Update index.js

--- story/index.js
+++ story/index.js
@@ -27,7 +27,7 @@ function initMap() {
     "The China–Nepal border runs across its summit point. " +
     "Its elevation (snow height) of 8,848.86 m (29,031 ft 8+1⁄2 in) was most recently " +
     "established in 2020 by the Chinese and Nepali authorities.</p>" +
-    "<p>Attribution: <a href="https://en.wikipedia.org/wiki/Mount_Everest>" +
+    "<p>Attribution: <a href=https://en.wikipedia.org/wiki/Mount_Everest>" +
   "https://en.wikipedia.org/wiki/Mount_Everest</p>" +
     "</div>" +
     "</div>";
